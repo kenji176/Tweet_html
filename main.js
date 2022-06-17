c5ab6178014ad5d660960e1eaad3c5ecd5ae0c20
sinc <73232388+kenji176@users.noreply.github.com>
2022-06-17 23:23:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,50 +6,37 @@ window.addEventListener('DOMContentLoaded', function () {
     const text = document.getElementById("text");
     const hash = document.getElementById("#");
 
-    function Name_change() {
-        const Name_ = document.getElementById("user_name");
-        Name_.textContent = Name.value
-    }
-
-    function Id_change() {
-        const Id_ = document.getElementById("user_id");
-        Id_.textContent = Id.value
-    }
-
-    function time_change() {
-        const Time_ = document.getElementById("Tweet_time");
-        Time_.textContent = time.value
-    }
-
-    function icon_change() {
-        const icon_ = document.getElementById("icon_image");
-        icon_.setAttribute('src', icon.value)
-    }
-
-    function text_change() {
-        const text_ = document.getElementById("tweet_text");
-        text_.innerHTML = text.value.replace(/\r?\n/g, '<br>');
-    }
-
-    function hash_change() {
-        const hashs = document.getElementById("hash_text");
-        hashs.innerHTML='';
-        const hash_text = hash.value.split(' ');
-        for (i = 0; i < hash_text.length; i++) {
-            console.log(hash_text[i]);
-            if (hash_text[i] == ""){
-                ;
-            }else{
-                hashs.insertAdjacentHTML('beforeend',"<span class='ms-1'>#"+hash_text[i]+"</span>");
+    function input_text(event) {
+        const ID = event.target.id;
+        if (ID == 'name') {
+            const Name_ = document.getElementById("user_name");
+            Name_.textContent = Name.value
+        } else if (ID == 'id') {
+            const Id_ = document.getElementById("user_id");
+            Id_.textContent = Id.value
+        } else if (ID == 'time') {
+            const Time_ = document.getElementById("Tweet_time");
+            Time_.textContent = time.value
+        } else if (ID == 'icon') {
+            const icon_ = document.getElementById("icon_image");
+            icon_.setAttribute('src', icon.value)
+        } else if (ID == '#') {
+            const hashs = document.getElementById("hash_text");
+            hashs.innerHTML = '';
+            const hash_text = hash.value.split(' ');
+            for (i = 0; i < hash_text.length; i++) {
+                console.log(hash_text[i]);
+                if (hash_text[i] == "") {
+                    ;
+                } else {
+                    hashs.insertAdjacentHTML('beforeend', "<span class='ms-1 mb-0 mt-0'>#" + hash_text[i] + "</span>");
+                }
             }
+        } else if(ID == 'text'){
+            const text_ = document.getElementById("tweet_text");
+            text_.innerHTML = text.value.replace(/\r?\n/g, '<br>');
         }
     }
 
-
-    Name.addEventListener('input', Name_change);
-    Id.addEventListener('input', Id_change);
-    time.addEventListener('input', time_change);
-    icon.addEventListener('input', icon_change);
-    text.addEventListener('input', text_change);
-    hash.addEventListener('input', hash_change);
+    addEventListener('input', input_text);
 })
