3b03e79bdbc5ff0245aac24cce37bf417eb83edf
sinc <73232388+kenji176@users.noreply.github.com>
2022-06-18 00:23:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ window.addEventListener('DOMContentLoaded', function () {
     const text = document.getElementById("text");
     const hash = document.getElementById("#");
 
+    const btn = document.getElementById("save");
+
     function input_text(event) {
         const ID = event.target.id;
         if (ID == 'name') {
@@ -19,6 +21,10 @@ window.addEventListener('DOMContentLoaded', function () {
             Time_.textContent = time.value
         } else if (ID == 'icon') {
             const icon_ = document.getElementById("icon_image");
+            toBase64Url(icon.value, function (base64Url) {
+                icon_.setAttribute('src', base64Url)
+            });
+
             icon_.setAttribute('src', icon.value)
         } else if (ID == '#') {
             const hashs = document.getElementById("hash_text");
@@ -32,11 +38,34 @@ window.addEventListener('DOMContentLoaded', function () {
                     hashs.insertAdjacentHTML('beforeend', "<span class='ms-1 mb-0 mt-0'>#" + hash_text[i] + "</span>");
                 }
             }
-        } else if(ID == 'text'){
+        } else if (ID == 'text') {
             const text_ = document.getElementById("tweet_text");
             text_.innerHTML = text.value.replace(/\r?\n/g, '<br>');
         }
     }
 
+    function toBase64Url(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                callback(reader.result);
+            }
+            reader.readAsDataURL(xhr.response);
+        };
+        xhr.open('GET', url);
+        xhr.responseType = 'blob';
+        xhr.send();
+    }
+
+    function Save_img() {
+        html2canvas(document.getElementById('Tweet')).then(canvas => {
+            let downloadEle = document.createElement("a");
+            downloadEle.href = canvas.toDataURL("image/png");
+            downloadEle.download = "canvas.png";
+            downloadEle.click();
+        });
+    }
+    btn.addEventListener('click', Save_img);
     addEventListener('input', input_text);
 })
